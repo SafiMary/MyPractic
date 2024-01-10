8bdef3130ec3a0940f20b4b1d26910e102fdc2fe
ADMIN <ADMIN@DESKTOP-CBRV7QC>
2024-01-10 21:14:46 +0500

    Добавьте файлы проекта.

---
+++ MyPractic/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace MyPractic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.labelInput = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.labelSearch = new System.Windows.Forms.Label();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.labelNewpath = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.labelReport = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.buttonSuspend = new System.Windows.Forms.Button();
+            this.buttonResume = new System.Windows.Forms.Button();
+            this.buttonAbort = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(33, 66);
+            this.textBoxInput.Multiline = true;
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(133, 20);
+            this.textBoxInput.TabIndex = 0;
+            // 
+            // labelInput
+            // 
+            this.labelInput.AutoSize = true;
+            this.labelInput.Location = new System.Drawing.Point(30, 50);
+            this.labelInput.Name = "labelInput";
+            this.labelInput.Size = new System.Drawing.Size(141, 13);
+            this.labelInput.TabIndex = 1;
+            this.labelInput.Text = "введите слова для поиска";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // labelSearch
+            // 
+            this.labelSearch.AutoSize = true;
+            this.labelSearch.Location = new System.Drawing.Point(33, 147);
+            this.labelSearch.Name = "labelSearch";
+            this.labelSearch.Size = new System.Drawing.Size(148, 13);
+            this.labelSearch.TabIndex = 2;
+            this.labelSearch.Text = "выберите папку для поиска";
+            this.labelSearch.Click += new System.EventHandler(this.labelSearch_Click);
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(76, 321);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 3;
+            this.buttonSearch.Text = "ПОИСК";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // labelNewpath
+            // 
+            this.labelNewpath.AutoSize = true;
+            this.labelNewpath.Location = new System.Drawing.Point(36, 214);
+            this.labelNewpath.Name = "labelNewpath";
+            this.labelNewpath.Size = new System.Drawing.Size(229, 13);
+            this.labelNewpath.TabIndex = 4;
+            this.labelNewpath.Text = "укажите папку, куда сохранить новый файл";
+            this.labelNewpath.Click += new System.EventHandler(this.labelNewpath_Click);
+            // 
+            // labelReport
+            // 
+            this.labelReport.AutoSize = true;
+            this.labelReport.Location = new System.Drawing.Point(36, 267);
+            this.labelReport.Name = "labelReport";
+            this.labelReport.Size = new System.Drawing.Size(230, 13);
+            this.labelReport.TabIndex = 5;
+            this.labelReport.Text = "укажите папку, куда сохранить файл отчета";
+            this.labelReport.Click += new System.EventHandler(this.labelReport_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(446, 107);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(279, 53);
+            this.progressBar.TabIndex = 6;
+            // 
+            // buttonSuspend
+            // 
+            this.buttonSuspend.Location = new System.Drawing.Point(423, 346);
+            this.buttonSuspend.Name = "buttonSuspend";
+            this.buttonSuspend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSuspend.TabIndex = 7;
+            this.buttonSuspend.Text = "приостановить";
+            this.buttonSuspend.UseVisualStyleBackColor = true;
+            // 
+            // buttonResume
+            // 
+            this.buttonResume.Location = new System.Drawing.Point(541, 346);
+            this.buttonResume.Name = "buttonResume";
+            this.buttonResume.Size = new System.Drawing.Size(75, 23);
+            this.buttonResume.TabIndex = 8;
+            this.buttonResume.Text = "возобновить";
+            this.buttonResume.UseVisualStyleBackColor = true;
+            // 
+            // buttonAbort
+            // 
+            this.buttonAbort.Location = new System.Drawing.Point(649, 345);
+            this.buttonAbort.Name = "buttonAbort";
+            this.buttonAbort.Size = new System.Drawing.Size(75, 23);
+            this.buttonAbort.TabIndex = 9;
+            this.buttonAbort.Text = "завершить";
+            this.buttonAbort.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonAbort);
+            this.Controls.Add(this.buttonResume);
+            this.Controls.Add(this.buttonSuspend);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.labelReport);
+            this.Controls.Add(this.labelNewpath);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.labelSearch);
+            this.Controls.Add(this.labelInput);
+            this.Controls.Add(this.textBoxInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Label labelInput;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label labelSearch;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Label labelNewpath;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label labelReport;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button buttonSuspend;
+        private System.Windows.Forms.Button buttonResume;
+        private System.Windows.Forms.Button buttonAbort;
+    }
+}
+
---
+++ MyPractic/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.LinkLabel;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace MyPractic
+{
+    public partial class Form1 : Form
+    {
+        string input;
+        string fileName;
+        string newpath;
+        string pathReport;
+        string DirName;
+        Thread MyThread; 
+   
+           
+        public Form1()
+        {
+            InitializeComponent();
+            progressBar.Step = 100;
+        }
+
+        private void labelSearch_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                fileName = openFileDialog1.FileName;
+            }
+            //using (var fbd = new FolderBrowserDialog())
+            //{
+
+            //    if (fbd.ShowDialog() == DialogResult.OK)
+            //    {
+            //        DirName = fbd.SelectedPath;
+            //    }
+            //}
+        }
+
+
+        public void searchWords()
+        {
+            //var contents = Directory.EnumerateFiles(DirName, "*.txt").Select(File.ReadAllLines).ToString(); //читать файлы только с таким расширением
+            //string content = contents;// сложили в строку
+            //Char[] Sep = { ' ', ',' };
+            //string str = textBoxInput.Text;
+            //string[] words = content.Split(Sep, StringSplitOptions.RemoveEmptyEntries);
+            //Console.WriteLine("Совпадений не найдено!"); //Выводим если совпадений не найдено
+            //foreach (string word in words)
+            //{
+            //    if (str.Equals(word))
+            //    {
+            //        Console.WriteLine("Есть совпадение!"); //Выводим если совпадения найдены
+            //        break;
+            //    }
+            //}
+            FileInfo fileInf = new FileInfo(fileName);
+            Char[] Sep = { ' ', ',' };
+            string str = textBoxInput.Text;
+            Regex regex = new Regex(str);
+            string target = "*******";
+            String[] arrString = str.Split(Sep);
+            string fileText = File.ReadAllText(fileName);//считали строки из файла
+            InfoMyFile();
+            foreach (string s in arrString) { 
+            if (fileText.Contains(s)) {
+                    
+                    fileInf.CopyTo(newpath, true);// копировали файл в новую папку
+                 //либо создать новый
+                StreamWriter file = new StreamWriter(newpath);
+                string result = regex.Replace(fileText, target);//наши слова заменяются на звездочки
+                //записать в него
+                file.Write(result);
+                //закрыть для сохранения данных
+                file.Close();
+                    Thread.Sleep(100);
+                progressBar.PerformStep();
+                  MessageBox.Show("Содержит данные слова");
+                }      
+                else MessageBox.Show("не содержит таких слов");
+            }
+            
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+         MyThread = new Thread(delegate () {
+            searchWords();
+         });
+            MyThread.Start();
+        }
+
+        private void labelNewpath_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                newpath = saveFileDialog1.FileName;
+            }
+        }
+
+        public void InfoMyFile()
+        {
+            StreamWriter file = new StreamWriter(pathReport);//путь куда сохранить
+            FileInfo files = new FileInfo(fileName);
+          
+            string directoryName = Path.GetDirectoryName(fileName);
+            long size = files.Length;
+            string namefile= files.Name;
+            
+            file.WriteLine($"Информация о файле {namefile}");
+            file.WriteLine($"Путь к файлу {directoryName}");
+            file.WriteLine($"Размер {size}");
+            file.Close();
+        }
+
+        private void labelReport_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pathReport = saveFileDialog1.FileName;
+            }
+        }
+
+        [Obsolete]
+        private void buttonSuspend_Click(object sender, EventArgs e)// приостановка
+        {
+                if (MyThread.IsAlive)
+                    MyThread.Suspend();
+        }
+
+
+        [Obsolete]
+        private void buttonResume_Click(object sender, EventArgs e)//возобновление
+        {
+                if (MyThread.ThreadState == ThreadState.Suspended)
+                MyThread.Suspend();
+            
+
+        }
+        [Obsolete]
+        private void buttonAbort_Click(object sender, EventArgs e)// завершение
+        {
+            progressBar.Value = 0;
+      
+                if (MyThread.IsAlive)
+                MyThread.Abort();
+            
+        }
+    }
+}
